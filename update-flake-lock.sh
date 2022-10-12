6f9fc6eab12d4c74c0b4309d31e458947be4c134
Judson Lester <nyarly@gmail.com>
2022-10-12 15:58:36 -0700

    Use gh CLI to make (verified) commit

--- update-flake-lock.sh
+++ update-flake-lock.sh
@@ -5,12 +5,18 @@ if [[ -n "$PATH_TO_FLAKE_DIR" ]]; then
   cd "$PATH_TO_FLAKE_DIR"
 fi
 
+commitArg=""
+
+if [[ "$COMMIT_WITH_TOKEN" != true ]]; then
+  commitArg="--commit-lock-file "
+fi
+
 if [[ -n "$TARGETS" ]]; then
     inputs=()
     for input in $TARGETS; do
         inputs+=("--update-input" "$input")
     done
-    nix flake lock "${inputs[@]}" --commit-lock-file --commit-lockfile-summary "$COMMIT_MSG"
+    nix flake lock "${inputs[@]}" $commitArg --commit-lockfile-summary "$COMMIT_MSG"
 else
-    nix flake update --commit-lock-file --commit-lockfile-summary "$COMMIT_MSG"
+    nix flake update $commitArg --commit-lockfile-summary "$COMMIT_MSG"
 fi
