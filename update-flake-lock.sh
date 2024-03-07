81892d6fc9f25853c6435eb67bc3d14012e95821
DavHau <hsngrmpf+github@gmail.com>
2024-03-07 12:13:29 +0700

    support updating multiple flake directories simultaneously
    This is useful for monorepos containing multiple flakes. For example it might be desired for a nixpkgs update to happen on all sub-flakes at the same time.
    
    Added a new option `flake-dirs` allowing to specify multiple flake directories to be updated simultaneously via a single PR

--- update-flake-lock.sh
+++ update-flake-lock.sh
@@ -1,23 +1,34 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ -n "$PATH_TO_FLAKE_DIR" ]]; then
-  cd "$PATH_TO_FLAKE_DIR"
-fi
+update(){
+  if [[ -n "${TARGETS:-}" ]]; then
+      inputs=()
+      for input in $TARGETS; do
+          inputs+=("--update-input" "$input")
+      done
+      nix "${options[@]}" flake lock "${inputs[@]}" --commit-lock-file --commit-lockfile-summary "$COMMIT_MSG"
+  else
+      nix "${options[@]}" flake update --commit-lock-file --commit-lockfile-summary "$COMMIT_MSG"
+  fi
+}
+
 
 options=()
-if [[ -n "$NIX_OPTIONS" ]]; then
+if [[ -n "${NIX_OPTIONS:-}" ]]; then
     for option in $NIX_OPTIONS; do
         options+=("${option}")
     done
 fi
 
-if [[ -n "$TARGETS" ]]; then
-    inputs=()
-    for input in $TARGETS; do
-        inputs+=("--update-input" "$input")
-    done
-    nix "${options[@]}" flake lock "${inputs[@]}" --commit-lock-file --commit-lockfile-summary "$COMMIT_MSG"
+if [[ -n "${PATH_TO_FLAKE_DIR:-}" ]]; then
+  cd "$PATH_TO_FLAKE_DIR"
+  update
+elif [[ -n "${FLAKE_DIRS:-}" ]]; then
+  for flake_dir in $FLAKE_DIRS; do
+    cd "$flake_dir"
+    update
+  done
 else
-    nix "${options[@]}" flake update --commit-lock-file --commit-lockfile-summary "$COMMIT_MSG"
+  update
 fi
