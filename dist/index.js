cf6776dfd17f4161ba21e58223dbe06110887997
Luc Perkins <lucperkins@gmail.com>
2024-04-21 19:17:03 -0300

    Add initial JS setup

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = new URL('.', import.meta.url).pathname.slice(import.meta.url.match(/^file:\/\/\/\w:/) ? 1 : 0, -1) + "/";
+/******/ 
+/************************************************************************/
+var __webpack_exports__ = {};
+// src/index.ts
+function main() {
+}
+main();
+//# sourceMappingURL=index.js.map
