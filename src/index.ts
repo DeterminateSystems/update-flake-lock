502daa7e5e63496ab7c627b4339033738fee5f5a
Luc Perkins <lucperkins@gmail.com>
2024-04-21 19:50:32 -0300

    Construct Nix command

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { ActionOptions, IdsToolbox, inputs } from "detsys-ts";
 class UpdateFlakeLockAction {
   idslib: IdsToolbox;
   private nixOptions: string;
-  private targets: string;
+  private targets: string[];
   private commitMessage: string;
   private pathToFlakeDir: string;
 
@@ -18,12 +18,19 @@ class UpdateFlakeLockAction {
     this.idslib = new IdsToolbox(options);
 
     this.nixOptions = inputs.getString("nix-options");
-    this.targets = inputs.getString("inputs");
+    this.targets = inputs.getString("inputs").split(" ");
     this.commitMessage = inputs.getString("commit-msg");
     this.pathToFlakeDir = inputs.getString("path-to-flake-dir");
   }
 
-  async update(): Promise<void> {}
+  async update(): Promise<void> {
+    const inputFlags = this.targets
+      .map((input) => `--update-input ${input}`)
+      .join(" ");
+    const inputStr = this.targets.length > 1 ? `${inputFlags}` : undefined;
+
+    const nixCommand = `nix ${this.nixOptions} flake lock ${inputStr} --commit-lock-file --commit-lock-file-summary "${this.commitMessage}"`;
+  }
 }
 
 function main(): void {
