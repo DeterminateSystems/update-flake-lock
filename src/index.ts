b1f8684b2117b1046e8bcaf25502699673d4d7b8
Luc Perkins <lucperkins@gmail.com>
2024-04-21 19:42:23 -0300

    Update Nix shell and add envrc

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,39 @@
-function main(): void {}
+import { ActionOptions, IdsToolbox, inputs } from "detsys-ts";
+
+class UpdateFlakeLockAction {
+  idslib: IdsToolbox;
+  private nixOptions: string;
+  private targets: string;
+  private commitMessage: string;
+  private pathToFlakeDir: string;
+
+  constructor() {
+    const options: ActionOptions = {
+      name: "update-flake-lock",
+      // We don't
+      fetchStyle: "universal",
+      requireNix: "fail",
+    };
+
+    this.idslib = new IdsToolbox(options);
+
+    this.nixOptions = inputs.getString("nix-options");
+    this.targets = inputs.getString("inputs");
+    this.commitMessage = inputs.getString("commit-msg");
+    this.pathToFlakeDir = inputs.getString("path-to-flake-dir");
+  }
+
+  async update(): Promise<void> {}
+}
+
+function main(): void {
+  const updateFlakeLock = new UpdateFlakeLockAction();
+
+  updateFlakeLock.idslib.onMain(async () => {
+    await updateFlakeLock.update();
+  });
+
+  updateFlakeLock.idslib.execute();
+}
 
 main();
