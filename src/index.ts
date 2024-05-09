28eac596e7c6196fcf8ef7070f7b4b65c1ca85e7
Graham Christensen <graham@grahamc.com>
2024-05-09 15:47:03 -0400

    nits on the nits

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ class UpdateFlakeLockAction {
 
   async update(): Promise<void> {
     // Nix command of this form:
-    // nix ${maybe nix options} flake ${update" or "lock"} ${maybe --update-input flags} --commit-lock-file --commit-lockfile-summary ${commit message}
+    // nix ${maybe nix options} flake ${"update" or "lock"} ${maybe --update-input flags} --commit-lock-file --commit-lockfile-summary ${commit message}
     // Example commands:
     // nix --extra-substituters https://example.com flake lock --update-input nixpkgs --commit-lock-file --commit-lockfile-summary "updated flake.lock"
     // nix flake update --commit-lock-file --commit-lockfile-summary "updated flake.lock"
