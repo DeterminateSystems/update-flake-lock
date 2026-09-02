66d249ddc890888e85b54446f00d279ea370ae12
Graham Christensen <graham@grahamc.com>
2026-09-02 17:26:29 -0400

    Depend on detsys-ts from npm
    The library is published as @determinate-systems/detsys-ts.
    Use the 1.x release line from the registry instead of a git reference.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { makeNixCommandArgs } from "./nix.js";
 import * as actionsCore from "@actions/core";
 import * as actionsExec from "@actions/exec";
-import { DetSysAction, inputs } from "detsys-ts";
+import { DetSysAction, inputs } from "@determinate-systems/detsys-ts";
 
 const EVENT_EXECUTION_FAILURE = "execution_failure";
 
